bb966dd76e96236958ee581ffd5ea8cd3cae2ec5
Ali Kmndlu <alikmndlu1@gmail.com>
2022-01-11 18:09:48 +0330

    OAuth2 protection feature

--- src/main/java/com/alikmndlu/gateway/GatewayApplication.java
+++ src/main/java/com/alikmndlu/gateway/GatewayApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
 
 
---
+++ src/main/java/com/alikmndlu/gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.alikmndlu.gateway.config;
+
+import com.alikmndlu.gateway.filter.JwtAuthenticationFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    private JwtAuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes().route("auth", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("lb://auth"))
+                .route("alert", r -> r.path("/alert/**").filters(f -> f.filter(filter)).uri("lb://alert"))
+                .route("echo", r -> r.path("/echo/**").filters(f -> f.filter(filter)).uri("lb://echo"))
+                .route("hello", r -> r.path("/hello/**").filters(f -> f.filter(filter)).uri("lb://hello")).build();
+    }
+
+}
+
